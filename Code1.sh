8f5d9ff5d134d20242d973f4a791b09471bc3090
AnuragBojja <anuragbojja19@gmail.com>
2026-01-21 14:21:12 -0600

    script code file

---
+++ Code1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1="Alice"
+PERSON2="Bob"
+echo "Hello, $PERSON1 and $PERSON2!"   
+echo "Welcome to the AWS Shell Scripting Tutorial."
+echo "This script demonstrates variable usage in bash."
+echo "Have a great day, $PERSON1 and $PERSON2!"
