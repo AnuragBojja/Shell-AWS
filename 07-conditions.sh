714983f6e2d136218157dceabffeb60c73e8c657
AnuragBojja <anuragbojja19@gmail.com>
2026-01-28 15:05:58 -0600

    commit

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -3,6 +3,8 @@
 Number=$1
 if [ $Number -lt 10 ]; then
     echo "given number $Number is less than 10"
+elif [ $Number -eq 10 ]; then
+    echo "given number $Number is equal to 10"
 else 
-    echo "given number $Number is grater than or equal to 10"
+    echo "given number $Number is grater than 10"
 fi
