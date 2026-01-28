8d481e7846c430d3276a01758a5926c279d06b02
AnuragBojja <anuragbojja19@gmail.com>
2026-01-28 16:19:48 -0600

    commit

--- 09-mysql.sh
+++ 09-mysql.sh
@@ -3,13 +3,15 @@
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then 
-    echo "run this with root privelege"
+    echo "ERROR: Run this with root privelege"
+    exit 1 
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then 
     echo "ERROR: installing mysql is failure"
+    exit 1
 else
     echo "Installing mysql is success"
 fi
