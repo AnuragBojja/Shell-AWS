277bbd728b6bb556adee340c042af2b1d5ceafc8
AnuragBojja <anuragbojja19@gmail.com>
2026-01-29 15:29:07 -0600

    Shell-Script

--- 12-logs2.sh
+++ 12-logs2.sh
@@ -13,44 +13,55 @@ echo "Log File Created at $LOGFILE"
 
 if [ "$USERID" -ne 0 ]; then
     echo "Run this file with Root Privilage" &>> "$LOGFILE"
+    echo "Run this file with Root Privilage"
     exit 1
 else 
     echo "This files running with Root Privilage" &>> "$LOGFILE"
+    echo "This files running with Root Privilage"
 fi
 
 VALIDATOR(){
     if [ "$1" -eq 0 ]; then
         echo "Installing $2 SUCCESSFUL" &>> "$LOGFILE"
+        echo -e "$Y Installing $2 SUCCESSFUL $N"
     else
-        echo -e "$R ERROR Installation $2 $N" &>> "$LOGFILE"
+        echo "ERROR Installation $2" &>> "$LOGFILE"
+        echo -e "$R ERROR Installation $2 $N"
         exit 1
     fi 
-    echo -e "$G ................................... $N" &>> "$LOGFILE"
+    echo " ................................... " &>> "$LOGFILE"
+    echo -e "$G ................................... $N"
 }
 
-echo -e "$G ................................... $N" &>> "$LOGFILE"
+echo " ................................... " &>> "$LOGFILE"
+echo -e "$G ................................... $N"
 dnf list installed mysql &>> "$LOGFILE"
 if [ $? -ne 0 ]; then
     dnf install mysql -y &>> "$LOGFILE"
     VALIDATOR $? "MySQL"
 else
-    echo -e "$G MySQL is Already Installed $N" &>> "$LOGFILE"
+    echo "MySQL is Already Installed" &>> "$LOGFILE"
+    echo -e "$G MySQL is Already Installed $N"
 fi
 
-echo -e "$G ................................... $N" &>> "$LOGFILE"
+echo "..................................." &>> "$LOGFILE"
+echo -e "$G ................................... $N"
 dnf list installed python3 &>> "$LOGFILE"
 if [ $? -ne 0 ]; then
     dnf install python3 -y &>> "$LOGFILE"
     VALIDATOR $? "Python3"
 else
-    echo -e "$G Python3 is Already Installed $N" &>> "$LOGFILE"
+    echo "Python3 is Already Installed" &>> "$LOGFILE"
+    echo -e "$G Python3 is Already Installed $N"
 fi
 
-echo -e "$G ................................... $N" &>> "$LOGFILE"
+echo "..................................." &>> "$LOGFILE"
+echo -e "$G ................................... $N"
 dnf list installed nginx &>> "$LOGFILE"
 if [ $? -ne 0 ]; then
     dnf install nginx -y &>> "$LOGFILE"
     VALIDATOR $? "Nginx"
 else
-    echo -e "$G Nginx is Already Installed $N" &>> "$LOGFILE"
+    echo "Nginx is Already Installed" &>> "$LOGFILE"
+    echo -e "$G Nginx is Already Installed $N"
 fi
