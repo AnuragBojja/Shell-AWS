6e911e09e622a4d0556a6d373b063f83b330a278
AnuragBojja <anuragbojja19@gmail.com>
2026-01-28 18:01:56 -0600

    commit

--- 10-installation.sh
+++ 10-installation.sh
@@ -4,5 +4,27 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-echo -e "$R Hello World $N"
-echo -e "$G test this $N"
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then 
+    echo -e "$R ERROR: Run this with root privelege $N"
+    exit 1 
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then 
+        echo -e "$R ERROR: installing $2 is failure $N"
+        exit 1
+    else
+        echo -e "$G Installing $2 is success$N"
+    fi
+}
+
+dnf install mysql -y
+VALIDATE $? "MySql"
+
+dnf install nginx -y
+VALIDATE $? "Nginx"
+
+dnf install python3 -y
+VALIDATE $? "python3"
