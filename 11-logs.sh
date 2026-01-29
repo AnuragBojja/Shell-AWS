692e1a0aa14ce18ed003413f63b8c54f38380fc9
AnuragBojja <anuragbojja19@gmail.com>
2026-01-28 20:17:23 -0600

    commit

--- 11-logs.sh
+++ 11-logs.sh
@@ -6,12 +6,12 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 LOGS_FOLDER="/var/log/shell-script"
-mkdir -p LOGS_FOLDER
+mkdir -p "$LOGS_FOLDER"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
-echo -e "$G This the log file of $PWD/$0 $N"
-echo -e "$Y Script started executed at $(date) $N" | tee $LOG_FILE
+echo -e "$G This the log file of $PWD/$0 $N" &>> $LOG_FILE 
+echo -e "$Y Script started executed at $(date) $N" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]; then 
     echo -e "$R ERROR: Run this with root privelege $N"
@@ -20,10 +20,14 @@ fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then 
-        echo -e "$R ERROR: installing $2 is failure $N" | tee $LOG_FILE
+        echo -e "$R ERROR: installing $2 is failure $N" | tee -a $LOG_FILE
+        echo -e "$Y Script started executed at $(date) $N" | tee -a $LOG_FILE
+    
         exit 1
     else
-        echo -e "$G Installing $2 is success$N" | tee $LOG_FILE
+        echo -e "$G Installing $2 is success$N" | tee -a $LOG_FILE
+        echo -e "$Y Script started executed at $(date) $N" | tee -a $LOG_FILE
+    
     fi
 }
 
@@ -33,7 +37,9 @@ if [ $? -ne 0 ]; then
     dnf install mysql -y &>>$LOG_FILE
     VALIDATE $? "MySql"
 else
-    echo -e "$Y MySql is Already Installed $N" | tee $LOG_FILE
+    echo -e "$Y MySql is Already Installed $N" | tee -a $LOG_FILE
+    echo -e "$Y Script started executed at $(date) $N" | tee -a $LOG_FILE
+
 fi 
 
 dnf list installed nginx &>>$LOG_FILE
@@ -42,7 +48,9 @@ if [ $? -ne 0 ]; then
     dnf install nginx -y &>>$LOG_FILE
     VALIDATE $? "Nginx"
 else
-    echo -e "$Y Nginx is Already Installed $N" | tee $LOG_FILE
+    echo -e "$Y Nginx is Already Installed $N" | tee -a $LOG_FILE
+    echo -e "$Y Script started executed at $(date) $N" | tee -a $LOG_FILE
+
 fi 
 
 dnf list installed python3 &>>$LOG_FILE
@@ -50,7 +58,9 @@ if [ $? -ne 0 ]; then
     dnf install python3 -y &>>$LOG_FILE
     VALIDATE $? "python3"
 else 
-    echo -e "$Y Python is Alredy Installed $N" | tee $LOG_FILE
+    echo -e "$Y Python is Alredy Installed $N" | tee -a $LOG_FILE
+    echo -e "$Y Script started executed at $(date) $N" | tee -a $LOG_FILE
+
 fi 
 
 echo "..."
