eb73224dd760497c4a37e6437ccdc1b7dd3a8865
AnuragBojja <anuragbojja19@gmail.com>
2026-01-29 14:55:23 -0600

    Shell-Script

--- 12-logs2.sh
+++ 12-logs2.sh
@@ -3,39 +3,32 @@
 USERID=$(id -u)
 
 LOGFOLDER="/var/log/shell-logs"
-mkdir -p $LOGFOLDER
+mkdir -p "$LOGFOLDER"
 LOGFILENAME=$( echo $0 | cut -d "." -f1)
 LOGFILE="$LOGFOLDER/$LOGFILENAME.log"
 echo "Log File Created at $LOGFILE"
 
 if [ $USERID -ne 0 ]; then
-    echo "Run this file with Root Privilage" &>> $LOGFILE
+    echo "Run this file with Root Privilage" &>> "$LOGFILE"
     exit 1
 else 
-    echo "This files running with Root Privilage" &>> $LOGFILE
+    echo "This files running with Root Privilage" &>> "$LOGFILE"
 fi
 
 VALIDATOR(){
     if [ $1 -eq 0 ]; then
-        echo "Installing $2 SUCCESSFUL" &>> $LOGFILE
+        echo "Installing $2 SUCCESSFUL" &>> "$LOGFILE"
     else
         echo "ERROR Installation $2"
     fi 
-    echo "...." &>> $LOGFILE
-    echo "...." &>> $LOGFILE
-    echo "...." &>> $LOGFILE
-    echo "...." &>> $LOGFILE
-    echo "...." &>> $LOGFILE
-    echo "...." &>> $LOGFILE
-    echo "...." &>> $LOGFILE
-    echo "...." &>> $LOGFILE
+    echo "..................................." &>> "$LOGFILE"
 }
 
-dnf install mysql -y &>> $LOGFILE
+dnf install mysql -y &>> "$LOGFILE"
 VALIDATOR $? "MySQL"
 
-dnf install python3 -y &>> $LOGFILE
+dnf install python3 -y &>> "$LOGFILE"
 VALIDATOR $? "Python3"
 
-dnf install nginx -y &>> $LOGFILE
+dnf install nginx -y &>> "$LOGFILE"
 VALIDATOR $? "Nginx"
