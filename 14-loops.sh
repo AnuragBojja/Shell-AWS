0b5165d7e0489c3f5243acef8e867eadba399f25
AnuragBojja <anuragbojja19@gmail.com>
2026-01-29 17:32:06 -0600

    Shell-Script

--- 14-loops.sh
+++ 14-loops.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
+USERID=$(id -u)
 
-for i in {1..20}
+if [ "$USERID" -ne 0 ]; then
+    echo "Please Run With Root Privilage"
+    exit 1
+else
+    echo "This file running with Root Privilage"
+fi
+
+VALIDATOR(){
+    if [ "$1" -ne 0 ]; then
+        echo "ERROR installing $2"
+        exit 1
+    else
+        echo "Installing $2 SUCCESS"
+    fi
+}
+
+
+for PACKAGE in $@
 do
-    echo $i
+    echo "$PACKAGE"
 done
