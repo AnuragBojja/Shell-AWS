f3519e89662cd8d644572f5e5ca6e38e96d079e9
AnuragBojja <anuragbojja19@gmail.com>
2026-01-29 15:13:02 -0600

    Shell-Script

--- 12-logs2.sh
+++ 12-logs2.sh
@@ -11,7 +11,7 @@ LOGFILENAME=$( echo $0 | cut -d "." -f1)
 LOGFILE="$LOGFOLDER/$LOGFILENAME.log"
 echo "Log File Created at $LOGFILE"
 
-if [ $USERID -ne 0 ]; then
+if [ "$USERID" -ne 0 ]; then
     echo "Run this file with Root Privilage" &>> "$LOGFILE"
     exit 1
 else 
@@ -19,37 +19,38 @@ else
 fi
 
 VALIDATOR(){
-    if [ $1 -eq 0 ]; then
+    if [ "$1" -eq 0 ]; then
         echo "Installing $2 SUCCESSFUL" &>> "$LOGFILE"
     else
-        echo "ERROR Installation $2"
+        echo -e "$R ERROR Installation $2 $N" &>> "$LOGFILE"
+        exit 1
     fi 
-    echo -g "$G ................................... $N" &>> "$LOGFILE"
+    echo -e "$G ................................... $N" &>> "$LOGFILE"
 }
 
-echo "$G ................................... $N" &>> "$LOGFILE"
+echo -e "$G ................................... $N" &>> "$LOGFILE"
 dnf list installed mysql &>> "$LOGFILE"
 if [ $? -ne 0 ]; then
     dnf install mysql -y &>> "$LOGFILE"
     VALIDATOR $? "MySQL"
 else
-    echo "$G MySQL is Already Installed $N" &>> "$LOGFILE"
+    echo -e "$G MySQL is Already Installed $N" &>> "$LOGFILE"
 fi
 
-echo "$G ................................... $N" &>> "$LOGFILE"
+echo -e "$G ................................... $N" &>> "$LOGFILE"
 dnf list installed python3 &>> "$LOGFILE"
 if [ $? -ne 0 ]; then
     dnf install python3 -y &>> "$LOGFILE"
     VALIDATOR $? "Python3"
 else
-    echo "$G Python3 is Already Installed $N" &>> "$LOGFILE"
+    echo -e "$G Python3 is Already Installed $N" &>> "$LOGFILE"
 fi
 
-echo "$G ................................... $N" &>> "$LOGFILE"
+echo -e "$G ................................... $N" &>> "$LOGFILE"
 dnf list installed nginx &>> "$LOGFILE"
 if [ $? -ne 0 ]; then
     dnf install nginx -y &>> "$LOGFILE"
     VALIDATOR $? "Nginx"
 else
-    echo "$G Nginx is Already Installed $N" &>> "$LOGFILE"
+    echo -e "$G Nginx is Already Installed $N" &>> "$LOGFILE"
 fi
