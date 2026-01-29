990ac405a8b7bc32497875a9824660d5aa053144
AnuragBojja <anuragbojja19@gmail.com>
2026-01-29 17:10:23 -0600

    Shell-Script

--- 13-install.sh
+++ 13-install.sh
@@ -4,6 +4,18 @@ USERID=$(id -u)
 
 if [ "$USERID" -ne 0 ]; then
     echo "Please Run With Root Privilage"
+    exit 1
 else
     echo "This file running with Root Privilage"
 fi
+
+VALIDATOR(){
+    if [ $1 -ne 0 ]; then
+        echo "ERROR installing $2"
+        exit 1
+    else
+        echo "Installing $2 SUCCESS"
+}
+
+dnf install mysql -y
+VALIDATOR $? "MySQL"
