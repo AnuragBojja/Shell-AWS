5c17ca12560cef0c6d3644f5e196b96e797a81c7
AnuragBojja <anuragbojja19@gmail.com>
2026-01-29 17:16:02 -0600

    Shell-Script

--- 13-install.sh
+++ 13-install.sh
@@ -10,7 +10,7 @@ else
 fi
 
 VALIDATOR(){
-    if [ $1 -ne 0 ]; then
+    if [ "$1" -ne 0 ]; then
         echo "ERROR installing $2"
         exit 1
     else
@@ -18,5 +18,14 @@ VALIDATOR(){
     fi
 }
 
-dnf install mysql -y
-VALIDATOR $? "MySQL"
+
+INSTALLING_APPLICATION(){
+    local pkg="$1"
+    dnf list installed "pkg"
+    if [ $? -ne 0 ]; then 
+        dnf install "pkg" -y
+        VALIDATOR $? "pkg"
+    else
+        echo "$pkg Already Installed"
+    fi 
+}
