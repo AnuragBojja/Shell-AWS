a5008518fdce1565efd5cbb3008591992f8c33a3
AnuragBojja <anuragbojja19@gmail.com>
2026-01-29 14:19:12 -0600

    Shell-Script

--- 12-logs2.sh
+++ 12-logs2.sh
@@ -10,11 +10,12 @@ if [ $USERID -ne 0 ]; then
 else 
     echo "This files running with Root User"
 fi
-
+VALIDATOR(){
+    if [ $1 -eq 0 ]; then
+        echo "Installing $2 SUCCESS"
+    else
+        echo "ERROR Installation $2"
+    fi 
+}
 dnf install mysql -y
-
-if [ $? -eq 0 ]; then
-    echo "Installing mysql SUCCESS"
-else
-    echo "ERROR Installation mysql"
-fi 
+VALIDATOR $? "MySQL"
