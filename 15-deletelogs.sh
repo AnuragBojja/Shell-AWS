c2241f6df1c4f91899fb631eb4052f94a6a7596e
AnuragBojja <anuragbojja19@gmail.com>
2026-02-09 17:21:28 -0600

    Shell-Script

---
+++ 15-deletelogs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+LOGFOLDER="/var/log/shell-logs"
+mkdir -p "$LOGFOLDER"
+LOGFILENAME=$( echo $0 | cut -d "." -f1)
+LOGFILE="$LOGFOLDER/$LOGFILENAME.log"
+echo "Script started executed at: $(date)"
+
+if [ "$USERID" -ne 0 ]; then
+    echo "Run this file with Root Privilage" &>> "$LOGFILE"
+    echo "Run this file with Root Privilage"
+    exit 1
+else 
+    echo "This files running with Root Privilage" &>> "$LOGFILE"
+    echo "This files running with Root Privilage"
+fi
