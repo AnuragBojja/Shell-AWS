fb1632f470f5e1dfa1bcbf655743afeab9c120ea
AnuragBojja <anuragbojja19@gmail.com>
2026-02-09 19:17:43 -0600

    Shell-Script

--- 16-deletelogfiles.sh
+++ 16-deletelogfiles.sh
@@ -11,7 +11,7 @@ LOGFILENAME=$( echo $0 | cut -d "." -f1)
 LOGFILE="$LOGFOLDER/$LOGFILENAME.log"
 SOURCE_DIR=($1)
 DEST_FILE=($2)
-FILE_TIMELINE=($3:+14)
+FILE_TIMELINE=$($3:+14)
 echo "Script started executed at: $(date)"
 
 if [ "$USERID" -ne 0 ]; then
@@ -22,10 +22,12 @@ else
     echo "This files running with Root Privilage" &>> "$LOGFILE"
     echo "This files running with Root Privilage"
 fi
-echo -e "$Y Give Input as <file-name> <source-dir> <dest-dir> <how-older-files>(Optional) default 14days older files $N"
-if [ $# -le 2 ]; then
-    echo -e "Please enter in this formate $Y <file-name> <source-dir> <dest-dir> <how-older-files>(Optional) default 14days older files $N"
+USAGE(){
+    echo -e "$Y USAGE::sudo sh <file-name>.sh <source-dir> <dest-dir> <how-older-files>(Optional) default 14days older files $N"
     exit 1
+}
+if [ $# -le 2 ]; then
+    USAGE
 fi
 if [ ! -d $SOURCE_DIR ]; then
     echo -e "$R There is no such directory as $SOURCE_DIR $N"
