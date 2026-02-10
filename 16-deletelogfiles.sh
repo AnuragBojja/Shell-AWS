585ec588cfe9c32272f65ce7c10189bfc7b0af3a
AnuragBojja <anuragbojja19@gmail.com>
2026-02-09 19:09:34 -0600

    Shell-Script

--- 16-deletelogfiles.sh
+++ 16-deletelogfiles.sh
@@ -22,9 +22,9 @@ else
     echo "This files running with Root Privilage" &>> "$LOGFILE"
     echo "This files running with Root Privilage"
 fi
-echo "Give Input as <file-name> <source-dir> <dest-dir> <how-older-files>(Optional) default 14days older files"
+echo "$Y Give Input as <file-name> <source-dir> <dest-dir> <how-older-files>(Optional) default 14days older files $N"
 if [ $# -le 2 ]; then
-    echo "Please enter in this formate $Y <file-name> <source-dir> <dest-dir> <how-older-files>(Optional) default 14days older files $N"
+    echo -e "Please enter in this formate $Y <file-name> <source-dir> <dest-dir> <how-older-files>(Optional) default 14days older files $N"
     exit 1
 fi
 if [ ! -d $SOURCE_DIR ]; then
