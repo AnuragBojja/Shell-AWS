00e0228287c172efe2cc211a609be982ab4099c9
AnuragBojja <anuragbojja19@gmail.com>
2026-02-09 19:33:24 -0600

    Shell-Script

--- 16-deletelogfiles.sh
+++ 16-deletelogfiles.sh
@@ -4,14 +4,14 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-USERID=($?)
+USERID="$?"
 LOGFOLDER="/var/log/shell-logs"
 mkdir -p "$LOGFOLDER"
 LOGFILENAME=$( echo $0 | cut -d "." -f1)
 LOGFILE="$LOGFOLDER/$LOGFILENAME.log"
-SOURCE_DIR=($1)
-DEST_FILE=($2)
-FILE_TIMELINE=${3:-14}
+SOURCE_DIR="$1"
+DEST_FILE="$2"
+DATE=${3:-14}
 echo "Script started executed at: $(date)"
 
 if [ "$USERID" -ne 0 ]; then
@@ -29,19 +29,19 @@ USAGE(){
 if [ $# -le 2 ]; then
     USAGE
 fi
-if [ ! -d $SOURCE_DIR ]; then
+if [ ! -d "$SOURCE_DIR" ]; then
     echo -e "$R There is no such directory as $SOURCE_DIR $N"
     exit 1
 fi
 echo -e "$Y $SOURCE_DIR Found $N"
-if [ ! -d $DEST_FILE ]; then
+if [ ! -d "$DEST_FILE" ]; then
     echo -e "$R There is no such directory as $DEST_FILE $N"
     exit 1
 fi
 echo -e "$Y $DEST_FILE Found $N"
-FILE_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -type f -mtime +"$FILE_TIMELINE")
+FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +"$DATE")
 
 while IFS= read -r file
 do
     echo "Files Found at $file"
-done <<< $FILE_TO_DELETE
+done <<< "$FILES"
