c54d9a1a8872c8c0fe701fa8873071976a15efcb
AnuragBojja <anuragbojja19@gmail.com>
2026-02-10 23:55:53 -0600

    Shell-Script

--- 18-mail.sh
+++ 18-mail.sh
@@ -4,11 +4,14 @@ SUBJECT=$2
 ALERT_TYPE=$3
 EMAIL_BODY=$4
 #FORMATTED_BODY=$(printf '%s\n' "$EMAIL_BODY" | sed -e 's/[]\/$*.^[]/\\&/g')
-FORMATTED_BODY=$(printf '%s' "$EMAIL_BODY" | sed -e 's/[\\&|]/\\&/g')
-IP_ADDRESS=$5
+# 1) Convert real newlines to <br> for HTML (keeps readability in mail)
+HTML_BODY=$(printf '%s' "$EMAIL_BODY" | sed 's/$/<br>/')
+
+# 2) Escape characters that break sed replacement: \, &, and the delimiter we will use (|)
+ESCAPED_BODY=$(printf '%s' "$HTML_BODY" | sed -e 's/[\\&|]/\\&/g')IP_ADDRESS=$5
 TO_TEAM=$6
 
-FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP_ADDRESS/g" -e "s/EMAIL_BODY/$FORMATTED_BODY/g" template.html)
+FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP_ADDRESS/g" -e "s/EMAIL_BODY/$ESCAPED_BODY/g" template.html)
 {
 echo "To: $TO_ADDRESS"
 echo "Subject: $SUBJECT"
