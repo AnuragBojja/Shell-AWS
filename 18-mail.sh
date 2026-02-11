823c8460824bf74801c92d40109409e52c8eeac1
AnuragBojja <anuragbojja19@gmail.com>
2026-02-10 23:51:01 -0600

    Shell-Script

--- 18-mail.sh
+++ 18-mail.sh
@@ -4,10 +4,11 @@ SUBJECT=$2
 ALERT_TYPE=$3
 EMAIL_BODY=$4
 #FORMATTED_BODY=$(printf '%s\n' "$EMAIL_BODY" | sed -e 's/[]\/$*.^[]/\\&/g')
+FORMATTED_BODY=$(printf '%s' "EMAIL_BODY")
 IP_ADDRESS=$5
 TO_TEAM=$6
 
-FINAL_BODY=$(sed "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP_ADDRESS/g" -e "s/EMAIL_BODY/$EMAIL_BODY/g" template.html)
+FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP_ADDRESS/g" -e "s/EMAIL_BODY/$FORMATTED_BODY/g" template.html)
 {
 echo "To: $TO_ADDRESS"
 echo "Subject: $SUBJECT"
