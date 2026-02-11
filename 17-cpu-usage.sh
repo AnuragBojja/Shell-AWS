28758d46420402bff95a11bd9431734726d4f0de
AnuragBojja <anuragbojja19@gmail.com>
2026-02-10 19:21:08 -0600

    Shell-Script

--- 17-cpu-usage.sh
+++ 17-cpu-usage.sh
@@ -11,8 +11,8 @@ do
     #echo "The usage of part $PART is $USAGE Per" 
     if [ $USAGE -gt $SPACE ]; then
         #echo "High Usage on $PART :: $USAGE Percentage"
-        MESSAGE+="High Usage from $IP_ADDRESS on $PART :: $USAGE %"
+        MESSAGE+="High Usage from $IP_ADDRESS on $PART :: $USAGE % \n"
     fi
 done <<< $DISK_USAGE
 
-echo "message body : $MESSAGE"
+echo -e "message body : $MESSAGE"
