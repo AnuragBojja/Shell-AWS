a63410aceb04a48d34c937028c23f2fa279e73dd
AnuragBojja <anuragbojja19@gmail.com>
2026-02-10 19:16:45 -0600

    Shell-Script

--- 17-cpu-usage.sh
+++ 17-cpu-usage.sh
@@ -2,12 +2,17 @@
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 SPACE=2
+IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+MESSAGE=""
 while IFS= read -r line
 do
     USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
     PART=$(echo $line | awk '{print $7}')
     #echo "The usage of part $PART is $USAGE Per" 
     if [ $USAGE -gt $SPACE ]; then
-        echo "High Usage on $PART :: $USAGE Percentage"
+        #echo "High Usage on $PART :: $USAGE Percentage"
+        MESSAGE="High Usage on $PART :: $USAGE %"
     fi
 done <<< $DISK_USAGE
+
+echo "message body : $MESSAGE"
