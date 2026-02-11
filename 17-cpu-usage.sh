bc4d16eef3a1bd9b33309d9cdb6746265cdc14b1
AnuragBojja <anuragbojja19@gmail.com>
2026-02-10 18:25:50 -0600

    Shell-Script

--- 17-cpu-usage.sh
+++ 17-cpu-usage.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-DISCK_USAGE=(df -hT | grep -v Filesystem)
+DISK_USAGE=(df -hT | grep -v Filesystem)
 
 while IFS= read -r line
 do
     echo "line $line"
 
-done <<< DISCK_USAGE
+done <<< $DISK_USAGE
