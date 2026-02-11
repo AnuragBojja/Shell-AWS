da7fda2e8d70cc18498429c5aa4b0d3c09a25572
AnuragBojja <anuragbojja19@gmail.com>
2026-02-11 00:08:57 -0600

    Shell-Script

--- 17-cpu-usage.sh
+++ 17-cpu-usage.sh
@@ -14,9 +14,9 @@ do
         MESSAGE+="High Usage from $IP_ADDRESS on $PART :: $USAGE % "$'\n'
     fi
 done <<< $DISK_USAGE
-#echo -e "message body : $MESSAGE"
-printf '%s' "$MESSAGE" #| sed -e 's/[]\/$*.^[]/\\&/g'
-sh 18-mail.sh "anuragbojja19@gmail.com" "High Disk Usage Alert" "High Disk Usage" "$MESSAGE" "$IP_ADDRESS" "DevOps Team"
+echo -e "message body : $MESSAGE"
+#printf '%s' "$MESSAGE" #| sed -e 's/[]\/$*.^[]/\\&/g'
+#sh 18-mail.sh "anuragbojja19@gmail.com" "High Disk Usage Alert" "High Disk Usage" "$MESSAGE" "$IP_ADDRESS" "DevOps Team"
 
 # TO_ADDRESS=$1
 # SUBJECT=$2
