f11c4507e1cde4fd6d10e454b3f93fec6a253a98
AnuragBojja <anuragbojja19@gmail.com>
2026-02-10 18:40:05 -0600

    Shell-Script

--- 17-cpu-usage.sh
+++ 17-cpu-usage.sh
@@ -7,7 +7,7 @@ do
     USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
     PART=$(echo $line | awk '{print $7}')
     #echo "The usage of part $PART is $USAGE Per" 
-    if [ $USAGE -gt SPACE ]; then
+    if [ $USAGE -gt $SPACE ]; then
         echo "High Usage on $PART :: $USAGE Percentage"
     fi
 done <<< $DISK_USAGE
